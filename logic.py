5fa634148059385624b34583178a1a06d743bdd1
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-13 11:50:35 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,3 +1,12 @@
 import random
+
 numberOfStreaks = 0
 for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    flips = ''.join(random.choice(['H', 'T']) for _ in range(100))
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+    if 'HHHHHH' in flips or 'TTTTTT' in flips:
+        numberOfStreaks += 1
+
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
